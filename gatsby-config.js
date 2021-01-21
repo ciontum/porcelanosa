6880d37685bfa291a442ff3440e6e67e7bd66a30
Dragos Gabriel Cireap <gabytza_dragos@yahoo.com>
2021-01-21 11:52:57 +0200

    Added gatsby-plugin-react-helmet

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,5 +31,6 @@ module.exports = {
         plugins: [],
       },
     },
-    `gatsby-transformer-remark-plaintext`],
+    `gatsby-transformer-remark-plaintext`,
+    `gatsby-plugin-react-helmet`],
 }
